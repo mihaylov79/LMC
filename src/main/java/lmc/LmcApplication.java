082753bc484d4e2e6c18839a75d51bc4bb923b82
Mihaylov <misho.mihaylov@gmail.com>
2025-10-02 01:27:23 +0300

    initial commit

---
+++ src/main/java/lmc/LmcApplication.java
@@ -0,0 +1,13 @@
+package java.lmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/lmc/LmcApplicationTests.java
@@ -0,0 +1,13 @@
+package java.lmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
