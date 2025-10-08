f76754b44b16cdc9e793df10d7b1fc08547cb426
Mihaylov <misho.mihaylov@gmail.com>
2025-10-09 01:10:51 +0300

    Update

--- src/main/java/lmc/config/SecurityConfig.java
+++ src/main/java/lmc/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package lmc.config;
 
+import lmc.user.model.User;
+import lmc.user.model.UserRole;
+import lmc.user.model.UserStatus;
+import lmc.user.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,4 +36,26 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
 
         return http.build();
     }
+ @Bean
+public CommandLineRunner initializeAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder){
+
+    return args -> {
+        if (userRepository.count() == 0) {
+
+            User admin = User.builder()
+                    .email("admin@local.me")
+                    .password(passwordEncoder.encode("admin321"))
+                    .firstName("Admin")
+                    .lastName("Deactivate after Permanent User is created")
+                    .userRole(UserRole.ADMIN)
+                    .status(UserStatus.ACTIVE)
+                    .build();
+
+            userRepository.save(admin);
+        }
+    };
+}
+
+
+
 }
